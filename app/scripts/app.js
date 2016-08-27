6ba8088f18269e81c79816dee6e1598015c7a2ab
vlgwlf <vitakub64@gmail.com>
2016-08-27 15:12:29 -0700

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name abbsbucketApp
+ * @description
+ * # abbsbucketApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('abbsbucketApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abbsbucketApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the abbsbucketApp
+ */
+angular.module('abbsbucketApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
